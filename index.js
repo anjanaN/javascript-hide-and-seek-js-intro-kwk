b94a946b2eb2dfcac1ce25a2ee55a44320ff5c6a
Anjana Nair <anju740@gmail.com>
2018-09-01 02:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function getFirstSelector(selector) {
+  return document.querySelector('selector');
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelectorAll('.target')
+}
+
+function increaseRankBy (n) {
+
+}
