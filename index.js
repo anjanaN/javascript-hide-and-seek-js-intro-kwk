17ce38f47ac99c53639de202c90f0521968a5eac
Anjana Nair <anju740@gmail.com>
2018-09-01 03:08:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ function nestedTarget() {
 }
 
 function increaseRankBy (n) {
-
+  const lis = document.getElementById('app').querySelectorAll('.ranked-list');
+  for (let i = 0; o < lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toString();
+  }
 }
